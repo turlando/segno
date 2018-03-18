f2949bb957d61bc82ca78867cd0663f4f3a091e7
Tancredi Orlando <tancredi.orlando@gmail.com>
2018-03-18 21:56:56 +0100

    Add translate and rotate

--- include/mutations.h
+++ include/mutations.h
@@ -1,3 +1,6 @@
 #pragma once
 
+struct mutation mutation_translate_x(double x);
+struct mutation mutation_translate_y(double y);
 struct mutation mutation_scale(double ratio);
+struct mutation mutation_rotate(double turns);
--- src/lang.c
+++ src/lang.c
@@ -12,13 +12,6 @@ SCM lang_polygon(SCM n_scm) {
     return shape_scm;
 }
 
-SCM lang_scale(SCM ratio_scm) {
-    double ratio = scm_to_double(ratio_scm);
-    struct mutation mutation = mutation_scale(ratio);
-    SCM mutation_scm = mutation_to_scm(mutation);
-    return mutation_scm;
-}
-
 SCM lang_combine(SCM mutations_scm) {
     SCM source_scm;
     struct mutation dest;
@@ -40,11 +33,42 @@ SCM lang_transform(SCM shape_scm, SCM mutation_scm) {
     return new_shape_scm;
 }
 
+SCM lang_translate_x(SCM x_scm) {
+    double x = scm_to_double(x_scm);
+    struct mutation mutation = mutation_translate_x(x);
+    SCM mutation_scm = mutation_to_scm(mutation);
+    return mutation_scm;
+}
+
+SCM lang_translate_y(SCM y_scm) {
+    double y = scm_to_double(y_scm);
+    struct mutation mutation = mutation_translate_y(y);
+    SCM mutation_scm = mutation_to_scm(mutation);
+    return mutation_scm;
+}
+
+SCM lang_scale(SCM ratio_scm) {
+    double ratio = scm_to_double(ratio_scm);
+    struct mutation mutation = mutation_scale(ratio);
+    SCM mutation_scm = mutation_to_scm(mutation);
+    return mutation_scm;
+}
+
+SCM lang_rotate(SCM turns_scm) {
+    double turns = scm_to_double(turns_scm);
+    struct mutation mutation = mutation_rotate(turns);
+    SCM mutation_scm = mutation_to_scm(mutation);
+    return mutation_scm;
+}
+
 static void lang_bind_primitives() {
     scm_c_define_gsubr("polygon",     1, 0, 0, &lang_polygon);
     scm_c_define_gsubr("transform",   2, 0, 0, &lang_transform);
     scm_c_define_gsubr("combine",     0, 0, 1, &lang_combine);
+    scm_c_define_gsubr("translate-x", 1, 0, 0, &lang_translate_x);
+    scm_c_define_gsubr("translate-y", 1, 0, 0, &lang_translate_y);
     scm_c_define_gsubr("scale",       1, 0, 0, &lang_scale);
+    scm_c_define_gsubr("rotate",      1, 0, 0, &lang_rotate);
 }
 
 static void lang_bind_draw() {
