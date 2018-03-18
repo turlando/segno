d8536d47d964d1988b59539bbd8efa158587b4ef
Tancredi Orlando <tancredi.orlando@gmail.com>
2018-03-18 16:51:47 +0100

    More window.c refactoring

--- src/window.c
+++ src/window.c
@@ -1,14 +1,28 @@
-#include <segno.h>
-#include <utils.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
 
-static void gl_init() {
+#include <window.h>
+#include <utils.h>
+#include <segno.h>
+
+static void glfw_init() {
     if (glfwInit() != GLFW_TRUE) {
         fprintf(stderr, "GLFW3: initialization failed\n");
         exit(EXIT_FAILURE);
     }
+
 }
 
-static void gl_window_init() {
+static void gl3w_init() {
+    if (gl3wInit()) {
+        fprintf(stderr, "GL3W: initialization failed\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void glfw_window_init() {
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -17,24 +31,36 @@ static void gl_window_init() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
 
-static GLFWwindow *window_new(int w, int h, const char *title) {
-    GLFWwindow *window = glfwCreateWindow(w, h, title, NULL, NULL);
-    global_window = window;
-
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);  // vsync
-
-    // TODO: if multiple windows use a static flag to call this just once
-    if (gl3wInit()) {
-        fprintf(stderr, "gl3w: failed to initialize\n");
+static GLFWwindow *window_new(int width, int height, const char *title) {
+    GLFWwindow *window = glfwCreateWindow(width, height, title, NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr, "GLFW3: window creation failed");
         exit(EXIT_FAILURE);
     }
-
     return window;
 }
 
-static Program shader_new() {
-    /* Shader sources */
+static void resize_callback(GLFWwindow* window, int width, int height) {
+    (void) window;
+    int side = MAX(width, height);
+    int x0 = (width - side) / 2;
+    int y0 = (height - side) / 2;
+
+    glViewport(x0, y0, side, side);
+}
+
+void window_loop() {
+    glfw_init();
+    glfw_window_init();
+
+    GLFWwindow *window = window_new(640, 640, "Segno");
+    glfwMakeContextCurrent(window);
+    glfwSetWindowSizeCallback(window, resize_callback);
+    glfwSwapInterval(1);  // vsync
+    gl3w_init();
+
+    global_window = window;
+
     const GLchar *vert_shader =
         "#version 330\n"
         "layout(location = 0) in vec2 point;\n"
@@ -49,38 +75,9 @@ static Program shader_new() {
         "    color = vec4(0.9, 0.9, 0.9, 0);\n"
         "}\n";
 
-    Program program = program_new(vert_shader, frag_shader);
-    return program;
-}
+    Program shader = program_new(vert_shader, frag_shader);
 
-static void gl_clean(Program program) {
-    program_free(program);
-    glfwTerminate();
-}
-
-static void size_callback(GLFWwindow* window, int width, int height) {
-    (void) window;
-    int side = MAX(width, height);
-    int x0 = (width - side) / 2;
-    int y0 = (height - side) / 2;
-
-    glViewport(x0, y0, side, side);
-}
-
-static void key_callback(GLFWwindow *window,
-                         int key, int scancode, int action, int mods) {
-    (void) scancode;
-    (void) mods;
-    if (key == GLFW_KEY_Q && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GL_TRUE);
-    }
-}
-
-static void gl_loop(GLFWwindow *window, Program shader) {
-    glfwSetWindowSizeCallback(window, size_callback);
-    glfwSetKeyCallback(window, key_callback);
-
-    while (!glfwWindowShouldClose(window)) {
+    while (glfwWindowShouldClose(window) != GLFW_TRUE) {
         glClearColor(0.1, 0.1, 0.1, 1);
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -90,16 +87,7 @@ static void gl_loop(GLFWwindow *window, Program shader) {
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-}
 
-void window_loop() {
-    gl_init();
-    gl_window_init();
-
-    GLFWwindow *window = window_new(640, 640, "Segno");
-    Program shader = shader_new();
-
-    gl_loop(window, shader);
-
-    gl_clean(shader);
+    program_free(shader);
+    glfwTerminate();
 }
