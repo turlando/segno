1fc2a39da03c109e061151bd954749e31a072e06
Tancredi Orlando <tancredi.orlando@gmail.com>
2018-03-19 19:19:43 +0100

    Give shader.c some love

--- src/shader.c
+++ src/shader.c
@@ -7,8 +7,10 @@ static GLuint compile(GLenum type, const GLchar *source) {
     GLuint shader = glCreateShader(type);
     glShaderSource(shader, 1, &source, NULL);
     glCompileShader(shader);
+
     GLint param;
     glGetShaderiv(shader, GL_COMPILE_STATUS, &param);
+
     if (!param) {
         GLchar log[4096];
         glGetShaderInfoLog(shader, sizeof(log), NULL, log);
@@ -16,6 +18,7 @@ static GLuint compile(GLenum type, const GLchar *source) {
                 type == GL_FRAGMENT_SHADER ? "frag" : "vert", (char *) log);
         exit(EXIT_FAILURE);
     }
+
     return shader;
 }
 
@@ -24,14 +27,17 @@ static GLuint link(GLuint vert, GLuint frag) {
     glAttachShader(program, vert);
     glAttachShader(program, frag);
     glLinkProgram(program);
+
     GLint param;
     glGetProgramiv(program, GL_LINK_STATUS, &param);
+
     if (!param) {
         GLchar log[4096];
         glGetProgramInfoLog(program, sizeof(log), NULL, log);
         fprintf(stderr, "GL3W: link: %s\n", (char *) log);
         exit(EXIT_FAILURE);
     }
+
     return program;
 }
 
