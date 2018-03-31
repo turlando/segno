bb16044bc2ab1c14eb3ba295d3220cee7346e9bf
Tancredi Orlando <tancredi.orlando@gmail.com>
2018-03-31 12:51:59 +0200

    Add documentation for vasnprintf

--- include/utils.h
+++ include/utils.h
@@ -6,6 +6,6 @@
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
-char *vansprintf(const char *fmt, ...);
+char *vasnprintf(const char *fmt, ...);
 
 #endif // __SEGNO_UTILS_H__
--- src/utils.c
+++ src/utils.c
@@ -3,6 +3,13 @@
 #include <stdarg.h>
 #include <utils.h>
 
+/**
+ * Return the pointer to an allocated string in a buffer
+ * large enough to hold the formatted string including the
+ * terminating NULL byte.
+ * The pointer should be passed to free(3) when the string
+ * is no longer needed.
+ */
 char *vasnprintf(const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
