8f953625313ecfe9c7d7c1252aa7c272956c2a2c
Tancredi Orlando <tancredi.orlando@gmail.com>
2018-04-17 00:20:10 +0200

    Update local Emacs configuration

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,11 @@
-(("src" . ((indent-tabs-mode . nil)
-           (tab-width . 4)
-           (fill-column . 80)
-           (c-mode . ((c-basic-offset . 4)
-                      (c-file-style . "BSD")))))
- ("include" . ((indent-tabs-mode . nil)
-               (tab-width . 4)
-               (fill-column . 80)
-               (c-mode . ((c-basic-offset . 4)
-                          (c-file-style . "BSD"))))))
+((c-mode . ((indent-tabs-mode . nil)
+            (tab-width . 4)))
+ (nil . ((c-file-style . "k&r")
+         (flycheck-clang-language-standard . "c99")
+         (flychck-clang-include-path ("src"
+                                      "include"))
+         (whitespace-style . (face
+                              tabs tab-mark indentation::tab
+                              spaces space-mark indentation::space
+                              newline newline-mark
+                              trailing)))))
